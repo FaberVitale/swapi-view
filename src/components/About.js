b7f0dfc0ed3c78ff0336561d6eec0acfe1c6a4c8
FaberVitale <faber.vitale@gmail.com>
2018-03-03 12:43:59 +0100

    added more info to /about

--- src/components/About.js
+++ src/components/About.js
@@ -22,7 +22,19 @@ class About extends React.Component<Props> {
             A <em>(progressive)</em> web app that displays data from{" "}
             <ExtLink href="https://swapi.co/">Swapi</ExtLink>
             {", a Star Wars restful API, created by Github user "}
-            <ExtLink href="https://github.com/phalt">Phalt</ExtLink>.
+            <ExtLink href="https://github.com/phalt">Phalt</ExtLink>.<br />
+            Created with <ExtLink href="https://reactjs.org/">
+              React
+            </ExtLink>{" "}
+            and the boiler-plate{" "}
+            <ExtLink href="https://github.com/facebook/create-react-app">
+              create-react-app
+            </ExtLink>.
+            <br />
+            {"Website source: "}
+            <ExtLink href="https://github.com/FaberVitale/swapi-view/">
+              {"github.com/FaberVitale/swapi-view/"}
+            </ExtLink>.
           </p>
         </section>
         <section className="about-section">
