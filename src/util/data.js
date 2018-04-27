bc402de05b6a1dca59341744ee5cb210f8b759ed
FaberVitale <faber.vitale@gmail.com>
2018-04-27 19:56:44 +0200

    fixed flow type errors

--- src/util/data.js
+++ src/util/data.js
@@ -5,25 +5,33 @@ const isLink = value =>
   typeof value !== "string" || /^https?|acaoaoakc/.test(value);
 
 const regex = /(\d+)\/?$/;
-export const mapTypeToRoute = (
+
+const UNMATCHED_ROUTE = "__UNMATCHED_ROUTE__";
+
+export const mapTypeToRoute: (
   type: string,
   endpoint: string,
   apiUrl: string,
   query?: { q: string, page: string } | null
-) => {
+) => string = (type, endpoint, apiUrl, query) => {
   const res = regex.exec(apiUrl);
 
-  if (type === "search" && query && res) {
-    return `/${endpoint}/search/${query.q}/${res[1]}`;
+  if (type === "search") {
+    if (query && res) {
+      return `/${endpoint}/search/${query.q}/${res[1]}`;
+    }
+    return UNMATCHED_ROUTE;
   }
 
-  return res && `/${endpoint}/${type}/${res[1]}`;
+  return res ? `/${endpoint}/${type}/${res[1]}` : UNMATCHED_ROUTE;
 };
 
-export const transformItem = (item: {}) =>
-  Object.keys(item || {}).reduce(
+export const transformItem: (item?: {}) => ItemData = item => {
+  const input = item || {};
+
+  return Object.keys(input).reduce(
     (aggr, key) => {
-      const value = item[key];
+      const value = input[key];
 
       if (isMetaKey(key)) {
         aggr.meta[key] = value;
@@ -36,24 +44,31 @@ export const transformItem = (item: {}) =>
     },
     { meta: {}, data: {}, links: {} }
   );
+};
 
-export const transformPage = (
+export const transformPage: (
   request: FetchRequest,
   page: {
     next: string | null,
     previous: string | null,
     results: Array<{ url: string }>
   }
-) => {
+) => * = (request, page) => {
   const type = request.type;
   const { endpoint } = request.match.params;
+
+  const ids: Array<string> = page.results.map(item =>
+    mapTypeToRoute("item", endpoint, item.url)
+  );
+
+  const results: Array<ItemData> = page.results.map(transformItem);
 
   return {
     next: page.next && mapTypeToRoute(type, endpoint, page.next, request.query),
     previous:
       page.previous &&
       mapTypeToRoute(type, endpoint, page.previous, request.query),
-    ids: page.results.map(item => mapTypeToRoute("item", endpoint, item.url)),
-    results: page.results.map(transformItem)
+    ids,
+    results
   };
 };
